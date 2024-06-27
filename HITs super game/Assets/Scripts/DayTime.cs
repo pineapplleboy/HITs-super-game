2fd7184d88baa4a4df422f58959677c2ca95cdcb
Aleksey Zinovjev <alexz04tab2@gmail.com>
2024-06-27 10:49:43 +0700

    add day/night changing

--- HITs super game/Assets/Scripts/DayTime.cs
+++ HITs super game/Assets/Scripts/DayTime.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json.Converters;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,90 @@ public class DayTime : MonoBehaviour
     public static int daysCounter;
     public static int nightsCounter;
 
+    private float dayTime = 36f;
+    private float nightTime = 12f;
+
+    private float raidTime = 24f;
+
+    private float secondsInHour = 1f;
+
+    private float secondsInHourDay;
+    private float secondsInHourNight;
+    private float secondsInHourRaid;
+
+    private bool isDay = true;
+
+    private int currentHours, currentMinutes;
+
+    private float currentTime;
+
+    private int raidFrequency = 3;
+
     void Start()
     {
         daysCounter = 0;
         nightsCounter = 0;
+
+        currentTime = 0;
+
+        secondsInHour = dayTime / 12;
+
+        secondsInHourDay = dayTime / 12;
+        secondsInHourNight = nightTime / 12;
+        secondsInHourRaid = raidTime / 12;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        currentTime += Time.deltaTime;
+
+        if (isDay)
+        {
+            currentHours = 12 + (int)(currentTime / secondsInHourDay);
+            currentMinutes = (int)((currentTime % secondsInHourDay) / (secondsInHourDay / 60));
+        }
+        else
+        {
+            if (Spawner.isAttack)
+            {
+                currentHours = (int)(currentTime / secondsInHourRaid);
+                currentMinutes = (int)((currentTime % secondsInHourRaid) / (secondsInHourRaid / 60));
+            }
+            else
+            {
+                currentHours = (int)(currentTime / secondsInHourNight);
+                currentMinutes = (int)((currentTime % secondsInHourNight) / (secondsInHourNight / 60));
+            }
+        }
+
+        if (isDay && currentTime >= dayTime)
+        {
+
+            isDay = false;
+            daysCounter++;
+            currentTime = 0;
+
+            if (daysCounter % raidFrequency == 0) Spawner.isAttack = true;
+
+            Debug.Log("night" + " " + GetTime());
+
+        }
+        else if (!isDay && currentTime >= nightTime)
+        {
+
+            isDay = true;
+            nightsCounter++;
+            currentTime = 0;
+
+            Spawner.isAttack = false;
+
+            Debug.Log("day" + " " + GetTime());
+
+        }
+    }
+
+    string GetTime()
+    {
+        return currentHours + " " + currentMinutes;
     }
 }
